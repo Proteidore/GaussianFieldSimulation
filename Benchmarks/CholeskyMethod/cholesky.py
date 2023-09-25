b1382236b0607e3626af699f65a47cad4971b187
Proteidore <126958818+Proteidore@users.noreply.github.com>
2023-09-25 14:54:32 +0200

    Update cholesky.py

--- Benchmarks/CholeskyMethod/cholesky.py
+++ Benchmarks/CholeskyMethod/cholesky.py
@@ -21,15 +21,15 @@ for dim in [1, 2, 3]:
         print("dim=", dim, "nb nodes=", mesh.getVerticesNumber(), "meanTime=", (t1 - t0) / num, "s")
  
 print("\n")  
-    for dim in [1, 2, 3]:
-       myCovModel=ot.ExponentialModel([1.0]*dim, [1.0])
-       for size in [10, 100, 1000]:
-           N = int(size**(1/dim)) #nombre de noeuds
-           for num in [250, 500, 750, 1000]: #nb de réalisations
-              mesh = ot.IntervalMesher([N-1]*dim).build(ot.Interval([-10.0]*dim, [10.0]*dim)) #maillage
-              process = ot.GaussianProcess(myCovModel, mesh) #processus gaussien
-              t0 = time.time()
-              sample=process.getSample(num) #échantillon de num réalisations du processus en les noeuds du maillage
-              t1 = time.time()
-              err=checkCovariance(sample, myCovModel) #calcul erreur L2
-              print("dim=", dim, "nb nodes=", mesh.getVerticesNumber(), "nb realisations=", num, "meanTime=", (t1 - t0) / num, "s", "errL2=",err)
+for dim in [1, 2, 3]:
+    myCovModel=ot.ExponentialModel([1.0]*dim, [1.0])
+    for size in [10, 100, 1000]:
+       N = int(size**(1/dim)) #nombre de noeuds
+       for num in [250, 500, 750, 1000]: #nb de réalisations
+          mesh = ot.IntervalMesher([N-1]*dim).build(ot.Interval([-10.0]*dim, [10.0]*dim)) #maillage
+          process = ot.GaussianProcess(myCovModel, mesh) #processus gaussien
+          t0 = time.time()
+          sample=process.getSample(num) #échantillon de num réalisations du processus en les noeuds du maillage
+          t1 = time.time()
+          err=checkCovariance(sample, myCovModel) #calcul erreur L2
+          print("dim=", dim, "nb nodes=", mesh.getVerticesNumber(), "nb realisations=", num, "meanTime=", (t1 - t0) / num, "s", "errL2=",err)
